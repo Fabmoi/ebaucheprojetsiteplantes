983abba336a279c370b0074416c3d336e0af0e93
Fabmoi <poulainfabian@gmail.com>
2021-04-30 15:01:25 +0200

    scrollmenu

--- link/bromeliacee/assets/js/onglets.js
+++ link/bromeliacee/assets/js/onglets.js
@@ -1,48 +1,80 @@
-(function(   ) {
-        /*
-        LORSQUE l'on clique sur un onglet
-            *ON RETIRE la class active de l'onglet actif
-            *J'ajoute la classe active à l'onglet actuel
-        
-        ON RETIRE la class active sur le contenu actif
-        J'ajoute la class active sur le contenu correspondant à mon clic
-        */
-        var afficherOnglet = function (a) {
-            var li = a.parentNode
-            var div = a.parentNode.parentNode.parentNode
-        
-            if(li.classList.contains('active')){
-                return false
-            }   
-            // ON RETIRE la class active de l'onglet actif
-            div.querySelector('.tabs .active').classList.remove('active')
-            //J'ajoute la classe active à l'onglet actuel
-            li.classList.add('active')
-        
-            //ON RETIRE la class active sur le contenu actif
-            div.querySelector('.tab-content.active').classList.remove('active')
-            //J'ajoute la class active sur le contenu correspondant à mon clic
-            div.querySelector(a.getAttribute('href')).classList.add('active')
-        }
-        var tabs = document.querySelectorAll('.tabs a')
-        for (var i = 0; i < tabs.length; i++){
-            tabs[i].addEventListener('click', function (e) {
-                afficherOnglet(this)
+class Tabs extends HTMLElement {
+
+    connectedCallback () {
+        this.setAttribute('role', 'tablist')
+        const tabs = Array.from(this.children)
+        const hash = window.location.hash.replace('#', '')
+        let currentTab =  tabs[0]
+
+        tabs.forEach((tab, i) => {
+            const id = tab.getAttribute('href').replace('#', '')
+            const tabpanel = document.getElementById(id)
+
+            if (id === hash) {
+                currentTab = tab
+            }
+            tab.setAttribute('role', 'tab')
+            tab.setAttribute('aria-selected', 'false')
+            tab.setAttribute('tabindex', '-1')
+            tab.setAttribute('aria-controls', id)
+            tab.setAttribute('id', 'tab-' + id)
+            tabpanel.setAttribute('role', 'tabpanel')
+            tabpanel.setAttribute('aria-labelledy', 'tab-' + id)
+            tabpanel.setAttribute('hidden', 'hidden')
+            tabpanel.setAttribute('tabindex', '0')
+            
+            tab.addEventListener('keyup', e => {
+                let index = null
+                if (e.key === 'ArrowRight') {
+                    index = i === tabs.length - 1 ? 0 : i + 1
+                 }else if (e.key === 'ArrowLeft') {
+                    index = i === 0 ? tabs.length - 1 : i - 1
+                } else if (e.key === 'Home') {
+                    index = 0  
+                } else if (e.key === 'End') {
+                    index = tabs.length - 1  
+                }
+                if (index !== null) {
+                    this.activate(tabs[index])
+                    tabs[index].focus()
+                }
             })
-        }   
-        /*
-        JE RECUPERE le hash
-        AJOUTER LA CLASS active sur le lien href="hash"
-        RETIRE LA CLASS active sur les autres onglets
-        AFFICHER / MASQUER les contenus
-        */
-        var hashChange = function (){
-            var hash = window.location.hash
-            var a = document.querySelector('a[href="' + hash + '"]')
-            if (a !== null && !a.classList.contains('active')) {
-                afficherOnglet(a)
-            }       
+            tab.addEventListener('click', e => {
+                e.preventDefault()
+                this.activate(tab)
+            })
+        })
+//activation onglet censé être actif
+         this.activate(currentTab, false)
+         if (currentTab.getAttribute('aria-controls') === hash)
+             window.requestAnimationFrame(() => {
+                 currentTab.scrollIntoView({
+                     behavior: 'smooth'
+                 })
+             })
+    }
+
+    /**
+     * @param {HTMLElement} tab
+     * @param {boolean} changehash
+     */
+    activate (tab, changeHash= true) {
+        const currentTab = this.querySelector('[aria-selected="true"]')
+        if (currentTab !==  null) {
+            const tabpanel = document.getElementById(currentTab.getAttribute('aria-controls'))
+            currentTab.setAttribute('aria-selected', 'false')
+            currentTab.setAttribute('tabindex', '-1')
+            tabpanel.setAttribute('hidden', 'hidden')
+
         }
-        window.addEventListener('hashchange, hashChange')
-        hashChange()
-    })()
+        const id = tab.getAttribute('aria-controls')
+        const tabpanel = document.getElementById(id)
+        tab.setAttribute('aria-selected', 'true')
+        tab.setAttribute('tabindex', '0')
+        tabpanel.removeAttribute('hidden')
+        if (changeHash) {
+            window.history.replaceState({}, '', '#' + id)
+        }
+    }
+
+}customElements.define('nav-tabs', Tabs)
